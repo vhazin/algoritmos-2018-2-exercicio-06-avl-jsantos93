93f560eba9cc9b23e6c136a2b7b7d8e67d3eff06
Jonathan Coutinho A Santos <37001764+jsantos93@users.noreply.github.com>
2018-09-23 18:57:23 -0300

    Update jcas.c

--- jcas.c
+++ jcas.c
@@ -1,64 +1,93 @@
 #include <stdio.h>
-
-int main(void)
-{
-    int t, n, valor;
-    int flag = 1;
-
-    scanf("%d", &t);
-    while (t--)
-    {
-        scanf("%d", &n);
-        int arvore[n];
-        for (int i = 0; i < n; i++)
-        {
-            scanf("%d", &valor);
-            arvore[i] = valor;
-        }
-        for (int j = 0; j < n; j++)
-        {
-            int direita = (2 * j) + 2;
-            int esquerda = (2 * j) + 1;
-
-            if (arvore[direita] == -1 && arvore[esquerda] != -1)
-            {
-                int esquerda_esquerda = (2 * esquerda) + 1;
-                int esquerda_direita = (2 * esquerda) + 2;
-
-                if (arvore[esquerda_esquerda] != -1 || arvore[esquerda_direita] != -1)
-                {
-                    flag = 0;
-                    break;
-                }
-            }
-            else if (arvore[esquerda] == -1 && arvore[direita] != -1)
-            {
-                int direita = (2 * j) + 2;
-                int esquerda = (2 * j) + 1;
-
-                if (arvore[direita] == -1 && arvore[esquerda] != -1)
-                {
-                    int direita_esquerda = (2 * direita) + 1;
-                    int direita_direita = (2 * direita) + 2;
-
-                    if (arvore[direita_esquerda] != -1 || arvore[direita_direita] != -1)
-                    {
-                        flag = 0;
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    if (flag)
-    {
-        printf("T\n");
-    }
-    else
-    {
-        printf("F\n");
-    }
-
+ 
+int minDepth(int a[], int root, int numeros){
+  int esq = 2*root+1;
+  int dir = 2*root+2;
+ 
+  if(a[root] == -1){
     return 0;
+  }
+ 
+  if(dir < numeros){
+    if(a[esq] == -1){
+      return 1;
+    }
+ 
+    if(a[dir] == -1 && a[esq] != 12){
+      return 1;
+    }
+ 
+    if(a[dir] == -1 && a[esq] == 12){
+      return 2;
+    }
+ 
+    if(minDepth(a, esq, numeros) >= minDepth(a, dir, numeros)){
+      return minDepth(a, dir, numeros) + 1;
+    }else{
+      return minDepth(a, esq, numeros) + 1;
+    }
+  }else{
+    if(esq < numeros){
+        return minDepth(a, esq, numeros) + 1;
+      }else{
+        return 1;
+    }
+  }
+}
+ 
+int maxDepth(int a[], int root, int numeros)  
+{
+  int esq = (2*root) + 1;
+  int dir = (2*root) + 2;
+ 
+  if (a[root] == -1){
+    return 0;
+  }
+  else
+  {
+    if(dir < numeros){
+      int lDepth = maxDepth(a, esq, numeros);
+      int rDepth = maxDepth(a, dir, numeros);
+   
+      if (lDepth > rDepth){
+        return(lDepth + 1);
+      }else{
+        return(rDepth + 1);
+      }
+    }
+    else{
+      if(esq < numeros){
+        int lDepth = maxDepth(a, esq, numeros);
+        return(lDepth + 1);
+      }
+      else{
+        return 1;
+      }
+    }
+  }
+}
+ 
+int main(void) {
+  int casos, numeros;
+ 
+  scanf("%d", &casos);
+ 
+  while(casos--){
+    scanf("%d", &numeros);
+    int tree[numeros];
+ 
+    for(int i = 0; i < numeros; i++){
+      scanf("%d", &tree[i]);
+    }
+ 
+    int min = minDepth(tree, 0, numeros);
+    int max = maxDepth(tree, 0, numeros);
+ 
+    if(max-min<=1){
+      printf("T\n");
+    }else{
+      printf("F\n");
+    }
+  }
+  return 0;
 }
